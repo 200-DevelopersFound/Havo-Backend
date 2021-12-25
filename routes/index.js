71e9076e216e3030d9a705f2c430196e3896b317
shreyans13 <shreyans1313@gmail.com>
2021-12-25 16:34:07 +0530

    hot-push/delete

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,11 @@
 const express = require("express");
 const sendMail = require("../api/nodemailer");
 const router = express.Router();
+// const { User } = require("../models");
+const User = require("../models/user");
+const BlacklistToken = require("../models/blacklistToken");
+const OTP = require("../models/otp");
+const UserLogin = require("../models/userLogin");
 
 /* GET home page. */
 router.get("/", (_req, res, _next) => {
@@ -8,12 +13,25 @@ router.get("/", (_req, res, _next) => {
   res.redirect("/api-docs");
 });
 
-// router.get("/send", (_req, res, next) => {
-//   sendMail("123:TEST", { message: "TestRun" }, (err, message) => {
-//     if (err) {
-//       return next(err);
-//     } else return res.json(message);
-//   });
-// });
+router.get("/deleteEverything", (_req, res, next) => {
+  let ob = {};
+  User.deleteMany({}, (err, payload) => {
+    if (err) ob["User"] = err;
+    else ob["User"] = payload;
+  });
+  BlacklistToken.deleteMany({}, (err, payload) => {
+    if (err) ob["BlacklistToken"] = err;
+    else ob["BlacklistToken"] = payload;
+  });
+  OTP.deleteMany({}, (err, payload) => {
+    if (err) ob["OTP"] = err;
+    else ob["OTP"] = payload;
+  });
+  UserLogin.deleteMany({}, (err, payload) => {
+    if (err) ob["UserLogin"] = err;
+    else ob["UserLogin"] = payload;
+  });
+  res.send(ob);
+});
 
 module.exports = router;
